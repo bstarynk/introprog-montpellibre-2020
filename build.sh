328e4dbadc071d1ba4099911d6c239e12eafa531
Basile Starynkevitch <basile@starynkevitch.net>
2020-07-30 19:09:46 +0200

    page de titre

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# file bismon/talks/Lamsade-21nov2019/build.sh
+# in http://github.com/bstarynk/bismon/
+for svgfile in *.svg ; do
+    svgbase=$(basename $svgfile .svg)
+    inkscape --without-gui --export-pdf=$svgbase.pdf $svgfile
+    inkscape --without-gui --export-eps=$svgbase.eps $svgfile
+done
+for dotfile in *.dot ; do
+    dotbase=$(basename $dotfile .dot)
+    dot -v -Teps -o $dotbase.eps  $dotfile
+    dot -v -Tpdf -o $dotbase.pdf  $dotfile
+    dot -v -Tsvg -o $dotbase.svg  $dotfile
+done
+lualatex --shell-escape  --halt-on-error intro-programmation-sous-Linux
+lualatex --shell-escape  --halt-on-error intro-programmation-sous-Linux
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#% Local Variables: ;;
+#% compile-command: "./build.sh" ;;
+#% End: ;;
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
